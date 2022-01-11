239c58e515827c7ae27a9cc06df6e4ce7e9efb6a
pacokwon <haechank@gmail.com>
2022-01-11 19:56:07 +0900

    feat: rewrite existing library functions on top of nextChar

--- app/Lib.hs
+++ app/Lib.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 module Lib where
 
 import Control.Applicative ((<|>))
@@ -18,16 +20,16 @@ nextChar :: Parser Char
 nextChar = Parser aux
   where
     aux :: InputState -> Either String (ParseResult Char)
-    aux s@InputState{ input, pos } =
+    aux s@InputState{ input, pos }
       | line' >= linesCount = Left "Expected character. Encountered EOF"
       | col' >= lineLength  = let nextLineNo = line' + 1
                                   nextLine   = input V.! nextLineNo
                                   position   = Position 1 nextLineNo
                                   inputState = InputState input position
-                               in ParseResult (head nextLine) inputState
+                               in Right $ ParseResult (head nextLine) inputState
       | otherwise           = let position   = Position (col' + 1) line'
                                   inputState = InputState input position
-                               in ParseResult (currentLine !! col') inputState
+                               in Right $ ParseResult (currentLine !! col') inputState
       where
         col'        = col pos
         line'       = line pos
@@ -35,77 +37,81 @@ nextChar = Parser aux
         currentLine = input V.! line'
         lineLength  = length currentLine
 
--- satisfy :: (Char -> Bool) -> Parser Char
--- satisfy predicate = Parser aux
---   where
---     aux :: InputState -> Either String (ParseResult Char)
---     aux s@InputState{ input, pos } =
---       where
---         (currentLine, col') = getLineAndPos s
+satisfy :: (Char -> Bool) -> Parser Char
+satisfy predicate = do
+  c <- nextChar
+  if predicate c
+     then return c
+     else Parser $ const $ Left $ c : " does not satisfy predicate"
 
--- satisfy :: (Char -> Bool) -> Parser Char
--- satisfy predicate = Parser $ \case
---   x : xs | predicate x -> Right $ ParseResult x xs
---   _ -> Left "[satisfy] Doesn't satisfy predicate"
+char :: Char -> Parser Char
+char c = satisfy (== c)
 
--- char :: Char -> Parser Char
--- char c = satisfy (== c)
+alpha :: Parser Char
+alpha = satisfy isAlpha
 
--- alpha :: Parser Char
--- alpha = satisfy isAlpha
+alphaNum :: Parser Char
+alphaNum = satisfy isAlphaNum
 
--- alphaNum :: Parser Char
--- alphaNum = satisfy isAlphaNum
+anyChar :: Parser Char
+anyChar = nextChar
 
--- anyChar :: Parser Char
--- anyChar = Parser $ \case
---   x : xs -> Right $ ParseResult x xs
---   _ -> Left "[anyChar] Input is empty!"
+str :: String -> Parser String
+str (p:ps) = do
+  c <- satisfy (== p)
+  cs <- str ps
+  return $ c:cs
 
--- str :: String -> Parser String
--- str prefix = Parser $ \input ->
---   case stripPrefix prefix input of
---     Just remaining -> Right $ ParseResult prefix remaining
---     Nothing -> Left "[str] Given string not found"
+digit :: Parser Char
+digit = satisfy isDigit
 
--- digit :: Parser Char
--- digit = satisfy isDigit
+oneOf :: [Char] -> Parser Char
+oneOf candidates = do
+  c <- nextChar
+  if elem c candidates
+     then return c
+     else Parser $ const $ Left "[oneOf] Candidate character not found"
 
--- oneOf :: [Char] -> Parser Char
--- oneOf candidates = Parser $ \case
---   x : xs | elem x candidates -> Right $ ParseResult x xs
---   _ -> Left "[oneOf] Candidate character not found"
+eof :: Parser ()
+eof = Parser aux
+  where
+    aux :: InputState -> Either String (ParseResult ())
+    aux s@InputState{ input, pos }
+      | isEof     = Right $ ParseResult () s
+      | otherwise = Left $ "Expected EOF. Encountered character at [" ++ (show $ line' + 1) ++ "," ++ (show col') ++ "]"
+      where
+        col'        = col pos
+        line'       = line pos
+        linesCount  = V.length input
+        currentLine = input V.! line'
+        lineLength  = length currentLine
+        isEof       = line' > linesCount || (line' == linesCount && col' >= lineLength)
 
--- eof :: Parser ()
--- eof = Parser $ \case
---   x : xs -> Left "[EOF] Expected EOF. Encountered input"
---   [] -> Right $ ParseResult () ""
+choice :: [Parser a] -> Parser a
+choice = foldr (<|>) err
+  where
+    err = Parser $ const $ Left "[choice] None of the given parsers work"
 
--- choice :: [Parser a] -> Parser a
--- choice = foldr (<|>) err
---   where
---     err = Parser $ \_ -> Left "[Choice] None of the given parsers work"
+many :: Parser a -> Parser [a]
+many p = many1 p <|> return []
 
--- many :: Parser a -> Parser [a]
--- many p = many1 p <|> return []
+many1 :: Parser a -> Parser [a]
+many1 p = do
+  first <- p
+  rest <- many p
+  return $ first : rest
 
--- many1 :: Parser a -> Parser [a]
--- many1 p = do
---   first <- p
---   rest <- many p
---   return $ first : rest
+sepBy :: Parser a -> Parser s -> Parser [a]
+sepBy p s = sepBy1 p s <|> return []
 
--- sepBy :: Parser a -> Parser s -> Parser [a]
--- sepBy p s = sepBy1 p s <|> return []
+sepBy1 :: Parser a -> Parser s -> Parser [a]
+sepBy1 p s = do
+  first <- p
+  rest <- many $ s >> p
+  return $ first : rest
 
--- sepBy1 :: Parser a -> Parser s -> Parser [a]
--- sepBy1 p s = do
---   first <- p
---   rest <- many $ s >> p
---   return $ first : rest
+space :: Parser Char
+space = satisfy isSpace
 
--- space :: Parser Char
--- space = satisfy isSpace
-
--- spaces :: Parser String
--- spaces = many space
+spaces :: Parser String
+spaces = many space
